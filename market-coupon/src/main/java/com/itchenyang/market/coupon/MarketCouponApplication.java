821004765c70ddf3909f9aa8a9d70ce558fc015a
kw520 <1151094976@qq.com>
2022-08-12 21:47:47 +0800

    nacos服务注册、配置中心以及网关服务搭建

--- market-coupon/src/main/java/com/itchenyang/market/coupon/MarketCouponApplication.java
+++ market-coupon/src/main/java/com/itchenyang/market/coupon/MarketCouponApplication.java
@@ -3,9 +3,11 @@ package com.itchenyang.market.coupon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope    // 配置中心配置修改后，实时刷新
 public class MarketCouponApplication {
 
     public static void main(String[] args) {
