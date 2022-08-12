8a2a9c225465261c5b0d31b13fe8eb0df1aab11f
kw520 <1151094976@qq.com>
2022-08-12 22:03:47 +0800

    网关服务

---
+++ market-gateway/src/test/java/com/itchenyang/gateway/MarketGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itchenyang.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
