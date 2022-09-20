d9582df8bee180b5a72a9352908b579e842a9b6c
kw520 <1151094976@qq.com>
2022-09-20 19:24:30 +0800

    fix: 商品上架, es检索

---
+++ market-search/src/test/java/com/itchenyang/market/search/MarketSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itchenyang.market.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
